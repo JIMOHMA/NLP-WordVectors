e2e2f51a9fb9f7a09aaf9adeafbe7421b1ee2836
JIMOHMA <mail4ayodele@gmail.com>
2023-02-14 18:18:27 -0500

    Co-Occurrence and Work Embedding

---
+++ Co-occurence_WordEmbeddings.py
@@ -0,0 +1,68 @@
+# All Import Statements Defined Here
+# Note: Do not add to this list.
+# All the dependencies you need, can be installed by running .
+# ----------------
+
+import sys
+assert sys.version_info[0]==3
+assert sys.version_info[1] >= 5
+
+from gensim.models import KeyedVectors
+from gensim.test.utils import datapath
+import pprint
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [10, 5]
+import nltk
+nltk.download('reuters')
+from nltk.corpus import reuters
+import numpy as np
+import random
+import scipy as sp
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA
+
+START_TOKEN = '<START>'
+END_TOKEN = '<END>'
+
+np.random.seed(0)
+random.seed(0)
+# ----------------
+
+
+def read_corpus(category="crude"):
+    """ Read files from the specified Reuter's category.
+        Params:
+            category (string): category name
+        Return:
+            list of lists, with words from each of the processed files
+    """
+    files = reuters.fileids(category)
+    return [[START_TOKEN] + [w.lower() for w in list(reuters.words(f))] + [END_TOKEN] for f in files]
+
+#### DISPLAYING TO SEE WHAT THE INFORMATION IN THE CORPUS LOOKS LIKE
+# reuters_corpus = read_corpus()
+# pprint.pprint(reuters_corpus[:3], compact=True, width=100)
+
+def distinct_word(corpus):
+    """ Determine a list of distinct words for the corpus.
+        Params:
+            corpus (list of list of strings): corpus of documents
+        Return:
+            corpus_words (list of strings): list of distinct words across the corpus, sorted (using python 'sorted' function)
+            num_corpus_words (integer): number of distinct words across the corpus
+    """
+    corpus_words = []
+    num_corpus_words = -1
+    
+    # remove words that have numbers in them
+    # remove symbols e.g @, ?, /, ., -, =
+    # HINT: Regex may help here
+    
+    temp_words = set()
+    for sentence in corpus:
+        for word in sentence:
+            temp_words.add(word)
+    corpus_words = sorted(list(temp_words))
+    num_corpus_words = len(corpus_words)
+    
+    return corpus_words, num_corpus_words
